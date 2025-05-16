72eb4097c5ea8891b2562b2aeb99f5a535107726
PiaCOS <elco@odoo.com>
2025-05-16 14:37:33 +0200

    [IMP] nvim: changed tabs stuff

--- nvim/lua/pia_conf/plugins.lua
+++ nvim/lua/pia_conf/plugins.lua
@@ -2,40 +2,40 @@
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- PLUGINS
 
 require("lazy").setup({
-  -- Theme
-  {
-    "xero/evangelion.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      overrides = {
-        keyword = { fg = "#00ff00", bg = "#222222", undercurl = true },
-        ["@boolean"] = { link = "Special" },
-      },
-    },
-  },
+	-- Theme
+	{
+		"xero/evangelion.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+				overrides = {
+				keyword = { fg = "#00ff00", bg = "#222222", undercurl = false },
+				["@boolean"] = { link = "Special" },
+			},
+		},
+	},
 
 	-- Telescope
 	{
-  	"nvim-telescope/telescope.nvim",
-  	dependencies = { "nvim-lua/plenary.nvim" },
-  	config = function()
-    	require("telescope").setup()
-  	end
+	"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+	require("telescope").setup()
+	end
 	},
 
   -- Comment plugin
