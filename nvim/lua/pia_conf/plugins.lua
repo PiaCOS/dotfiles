7235c4c288d8ff8c0d58d36666d246ed99bbe6c1
PiaCOS <elco@odoo.com>
2025-05-28 17:32:35 +0200

    [IMP] nvim: add telescope live grep args plugin and config

--- nvim/lua/pia_conf/plugins.lua
+++ nvim/lua/pia_conf/plugins.lua
@@ -37,10 +37,58 @@ require("lazy").setup({
 	-- Telescope
 	{
 		"nvim-telescope/telescope.nvim",
-			dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-		require("telescope").setup()
-		end
+
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-live-grep-args.nvim",
+		},
+
+		opts = function(_, opts)
+			local lga_actions = require("telescope-live-grep-args.actions")
+			local vertical_layout = {
+				width = 0.8,
+				height = 0.8,
+				mirror = true,
+			}
+			opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+				layout_strategy = "vertical",
+				layout_config = vertical_layout,
+				winblend = 10,
+				previewer = false,
+			})
+			opts.extensions = {
+				live_grep_args = {
+					auto_quoting = true,
+					mappings = {
+						i = {
+							["<C-k>"] = lga_actions.quote_prompt(),
+							["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+						},
+					},
+					theme = "vertical",
+					layout_config = vertical_layout,
+				},
+			}
+		end,
+
+		keys = {
+			{
+				"<leader>ff",
+				function() require("telescope.builtin").find_files() end,
+				desc = "Find files",
+			},
+			{
+				"<leader>fg",
+				function() require("telescope").extensions.live_grep_args.live_grep_args() end,
+				desc = "Grep (live with args)"
+			},
+		},
+
+		config = function(_, opts)
+			local tele = require("telescope")
+			tele.setup(opts)
+			tele.load_extension("live_grep_args")
+		end,
 	},
 
 	-- Treesitter
@@ -49,7 +97,7 @@ require("lazy").setup({
 		build = ':TSUpdate',
 		config = function()
 			require'nvim-treesitter.configs'.setup {
-				ensure_installed = { "javascript", "typescript", "lua", "json" }, -- add more if needed
+				ensure_installed = { "javascript", "typescript", "lua", "json", "python", "rust", "fish", "xml" }, -- add more if needed
 				highlight = {
 					enable = true,
 					disable = { "markdown", },
@@ -57,14 +105,6 @@ require("lazy").setup({
 				},
 			}
 		end
-	},
-
-
-	-- Comment plugin
-	{
-		"numToStr/Comment.nvim",
-		opts = {},
-		lazy = false,
 	},
 })
 
