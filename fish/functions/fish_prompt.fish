7720c48da67bb162973b24160da09b12c284829e
PiaCOS <pia.cosneau@gmail.com>
2025-10-29 16:14:05 +0100

    imp: fish prompt update :sunflower: + new patch maplemono

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,30 +2,37 @@ function fish_prompt
     if not set -q VIRTUAL_ENV_DISABLE_PROMPT
         set -g VIRTUAL_ENV_DISABLE_PROMPT true
     end
+
+    # ----------------------------------------
+    # ---------------- Line 1 ----------------
+    # ----------------------------------------
+
     set_color normal
     printf 'λ '
+
+    if test -n "$VIRTUAL_ENV"
+        printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+    end
+
     set_color magenta
     printf ':: '
-
     set_color yellow
     printf '%s' $USER
     set_color normal
     printf ' -> '
 
-    # set_color magenta
-    # echo -n (prompt_hostname)
-    # set_color normal
-    # printf ' in '
-
     set_color $fish_color_cwd
     printf '%s' (prompt_pwd)
     set_color normal
 
-    # Line 2
+    # ----------------------------------------
+    # ---------------- Line 2 ----------------
+    # ----------------------------------------
+
+    # New line
     echo
-    if test -n "$VIRTUAL_ENV"
-        printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
-    end
-    printf '∇ '
+
+    # printf '∇ '
+    printf '::|> '
     set_color normal
 end
