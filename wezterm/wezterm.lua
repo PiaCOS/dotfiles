e5460b38e1e705861e93f3d1e5feda8b3611341a
PiaCOS <pia.cosneau@gmail.com>
2025-12-20 20:30:08 +0100

    wezterm: theme Seoul256

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -18,18 +18,20 @@ config.font = wezterm.font(FONT_FAMILY)
 --                      STYLING
 -- ----------------------------------------------------
 
-config.window_background_opacity = 0.94
+config.window_background_opacity = 0.89
 
 config.enable_tab_bar = false
 -- Define a custom scheme with Pastel Red text and Soft Black background
 config.color_schemes = {
 }
 -- config.color_scheme = 'Andromeda'
--- config.color_scheme = 'rose-pine'
-config.color_scheme = 'Atelier Plateau (base16)'
+-- config.color_scheme = 'rose-pine-moon'
+config.color_scheme = 'Seoul256 (Gogh)'
+-- config.color_scheme = 'Atelier Plateau (base16)'
 config.colors = {
   -- foreground = "#756d6d",
   foreground = "#d5cdcd",
+  background = "222222",
 }
 -- ----------------------------------------------------
 --                       UTILS
