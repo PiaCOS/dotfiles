18e7253db18449180d5b89895e1b88b84d0a313a
PiaCOS <pia.cosneau@gmail.com>
2026-01-10 10:38:44 +0100

    wezterm: config moved to the flake.nix

--- flake.nix
+++ flake.nix
@@ -155,6 +155,44 @@
             theme = "${pkgs.rofi}/share/rofi/themes/gruvbox-dark.rasi";
           };
 
+          # ---------------- wezterm config ----------------
+
+          xdg.configFile."wezterm/wezterm.lua".text = ''
+            local wezterm = require 'wezterm'
+            local config = {}
+
+            -- -------- FONTS --------
+            local FONT_FAMILY = "Maple Mono NF"
+            local FONT_SIZE = 9.5
+            config.font_size = FONT_SIZE
+            config.font = wezterm.font(FONT_FAMILY)
+
+            -- -------- THEME --------
+            config.window_background_opacity = 0.89
+            config.enable_tab_bar = false
+            config.color_schemes = {}
+            config.color_scheme = 'Seoul256 (Gogh)'
+            config.colors = {
+              foreground = "#d5cdcd",
+              background = "222222",
+            }
+
+            -- -------- SHELL --------
+            config.default_prog = { "fish" }
+
+            -- -------- KEYS --------
+            config.keys = {
+              -- was conflicting with lazygit commit keymap
+              {
+                key = 'Enter',
+                mods = 'ALT',
+                action = wezterm.action.DisableDefaultAssignment,
+              },
+            }
+
+            return config
+          '';
+
           # ---------------- niri config ----------------
 
           xdg.configFile."niri/config.kdl".text = ''
