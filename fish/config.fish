2ef18b7b1c816d7402c1f59e09e1d93e3cf55b53
PiaCOS <pia.cosneau@gmail.com>
2026-04-03 23:18:24 +0200

    fish: cleaning + command to sudo prev command

--- fish/config.fish
+++ fish/config.fish
@@ -65,6 +65,13 @@ if status is-interactive
     alias deathnote "ps -ef | fzf -m | awk '{print \$2}' | creepy"
     alias instakill "pasta | xargs -p kill -9; xclip -selection clipboard /dev/null"
 
+    # Politly execute the last command as root (press space to see what will be executed)
+    function _sudo_last
+        set last_command (history -n 1)
+        echo sudo $last_command
+    end
+    abbr -a please --position command --function _sudo_last
+
     # cool stuff
     function mkcd
         set arg $argv[1]
@@ -99,10 +106,6 @@ if status is-interactive
     # SSH Agent (start once per session)
     # ---------------------------------------------------
 
-    function start_agent
-        ssh-agent -c | tee ~/.ssh/agent_env_fish | source
-    end
-
     set -l agent_env_file ~/.ssh/agent_env_fish
 
     if test -f $agent_env_file
@@ -111,7 +114,9 @@ if status is-interactive
 
     if not set -q SSH_AGENT_PID; or not kill -0 $SSH_AGENT_PID 2>/dev/null; or not test -S "$SSH_AUTH_SOCK"
         echo "Starting new ssh-agent..."
-        ssh-agent -c | tee $agent_env_file | source
+        # ssh-agent -c | tee $agent_env_file | source
+        # silence the 'Agent PID blablabla'
+        ssh-agent -c | sed 's/^echo/#echo/' | tee $agent_env_file | source
     end
 
     if status is-interactive
