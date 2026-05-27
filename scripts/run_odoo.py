72bf2e7de47c395562931f72c625bcb233221797
PiaCOS <pia.cosneau@gmail.com>
2026-05-27 17:19:46 +0200

    script: run_odoo last modif before del

--- scripts/run_odoo.py
+++ scripts/run_odoo.py
@@ -66,26 +66,26 @@ to_update =  None
 to_test =    None
 
 arg_list = [
-    ArgField( "--addons",      "addons",          None ),
-    ArgField( "--dev",         "dev",             dev ),
-    ArgField( "-p",            "port",            port ),
-    ArgField( "-d",            "db",              db ),
-    ArgField( "-c",            "cwd",             cwd ),
-    ArgField( "-i",            "to_install",      to_install ),
-    ArgField( "-u",            "to_update",       to_update ),
-    ArgField( "--workers",     "workers",         workers ),
-    ArgField( "--test",        "to_test",         to_test ),
-    ArgField( "--log",         "log",             log ),
-    ArgField( "--db_host",     "db_host",         "localhost" ),
-    ArgField( "--db_user",     "db_user",         "odoo" ),
-    ArgField( "--db_password", "db_password",     "odoo" ),
-    ArgFlag( "-e",             "use_enterprise",  False ),
-    ArgFlag( "--demo",         "with_demo",       False ),
-    ArgFlag( "--log_sql",      "log_sql",         False ),
-    ArgFlag( "--shell",        "shell_mode",      False ),
-    ArgFlag( "--dry",          "dry_run",         False ),
-    ArgFlag( "--out",          "out",             False ),
-    ArgFlag( "--need_help",    "need_help",       False ),
+    ArgField( "--addons",       "addons",         None ),
+    ArgField( "--dev",          "dev",            dev ),
+    ArgField( "-p",             "port",           port ),
+    ArgField( "-d",             "db",             db ),
+    ArgField( "-c",             "cwd",            cwd ),
+    ArgField( "-i",             "to_install",     to_install ),
+    ArgField( "-u",             "to_update",      to_update ),
+    ArgField( "--workers",      "workers",        workers ),
+    ArgField( "--test",         "to_test",        to_test ),
+    ArgField( "--log",          "log",            log ),
+    ArgField( "--db_host",      "db_host",        "localhost" ),
+    ArgField( "--db_user",      "db_user",        "odoo" ),
+    ArgField( "--db_password",  "db_pwd",         "odoo" ),
+    ArgFlag( "-e",              "use_enterprise",  False ),
+    ArgFlag( "--demo",          "with_demo",       False ),
+    ArgFlag( "--log_sql",       "log_sql",         False ),
+    ArgFlag( "--shell",         "shell_mode",      False ),
+    ArgFlag( "--dry",           "dry_run",         False ),
+    ArgFlag( "--out",           "out",             False ),
+    ArgFlag( "--need_help",     "need_help",       False ),
 ]
 
 
@@ -135,8 +135,9 @@ def main():
 
     if not args.need_help:
         print("sys.executable: ", sys.executable)
-        command = f"{sys.executable} ./odoo-bin --addons-path={addons_path(args)} {mode} --dev={args.dev} --http-port={args.port} -d {args.db} --log-level={args.log}"
-        command += f" --db_host={args.db_host} --db_user={args.db_user} --db_password={args.db_password} --workers={args.workers}"
+        command = f"{sys.executable} ./odoo-bin --addons-path={addons_path(args)} {mode} --dev={args.dev} --http-port={args.port}"
+        command += f" --log-level={args.log}"
+        command += f" --db_host={args.db_host} --db_user={args.db_user} --db_password={args.db_pwd} --workers={args.workers}"
         if args.to_install:
             command += f" -i {args.to_install}"
         if args.to_update:
