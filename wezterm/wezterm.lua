08631ec68c555ff482e956726504e4f0c23b2b24
PiaCOS <pia.cosneau@gmail.com>
2026-06-10 11:06:41 +0200

    wezterm: open window in helix + cmd at startup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,37 @@
 
 local wezterm = require 'wezterm'
 
+local io = require 'io'
+local os = require 'os'
+
 local config = {}
+
+-- ----------------------------------------------------
+--                   NEW FUNCS
+-- ----------------------------------------------------
+
+-- wezterm.on('gui-startup', function(cmd)
+--   -- Run a command silently in the background
+--   wezterm.background_child_process { 'cat', '~/Dev/dotfiles/kan' }
+-- end)
+
+-- Open window in helix
+wezterm.on('edit-scrollback', function(window, pane)
+  local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+
+  local name = os.tmpname() .. ".fish"
+  local f = io.open(name, 'w+')
+  f:write(text)
+  f:flush()
+  f:close()
+
+  window:perform_action(
+    wezterm.action.SpawnCommandInNewTab {
+      args = { 'hx', name .. ':99999999' },
+    },
+    pane
+  )
+end)
 
 -- ----------------------------------------------------
 --                     FONT STUFF
@@ -44,13 +74,31 @@ config.default_prog = { "/usr/bin/fish" }
 
 config.keys = {
   -- was conflicting with lazygit commit keymap
+  -- {
+  --   key = 'Enter',
+  --   mods = 'ALT',
+  --   action = wezterm.action.DisableDefaultAssignment,
+  -- },
+  -- quick select but on every word
+  {
+    key = 'j',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.QuickSelectArgs {
+      label = 'copy word',
+      patterns = { '\\S+' },
+    },
+  },
+  -- open window in helix
   {
     key = 'Enter',
+    -- mods = 'ALT|SHIFT',
     mods = 'ALT',
-    action = wezterm.action.DisableDefaultAssignment,
+    action = wezterm.action.EmitEvent 'edit-scrollback',
   },
 }
 
+-- ----------------------------------------------------
+-- ----------------------------------------------------
 -- ----------------------------------------------------
 
 return config
